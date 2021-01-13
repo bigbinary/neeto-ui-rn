2aa94d878a680ec5fc771cec19cd694ee4a572b1
Ajay Sivan <ajayesivan@gmail.com>
2021-01-13 13:01:36 +0530

    ESlint & Husky precommit hook

--- docs/storybook/stories/Button.stories.js
+++ docs/storybook/stories/Button.stories.js
@@ -1,13 +1,9 @@
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react-native';
+import {action} from '@storybook/addon-actions';
+import {text} from '@storybook/addon-knobs';
+import {storiesOf} from '@storybook/react-native';
 import React from 'react';
-import { Button } from '@neeto-ui-rn/ui';
+import {Button} from '@neeto-ui-rn/ui';
 
 storiesOf('Button', module)
-  .add('Cool', () => (
-    <Button text="Cool" bg="red"/>
-  ))
-  .add('Nice', () => (
-    <Button text="Nice" backgroundColor="green"/>
-  ));
+  .add('Cool', () => <Button text="Cool" bg="red" />)
+  .add('Nice', () => <Button text="Nice" backgroundColor="green" />);
--- ui/components/Button.js
+++ ui/components/Button.js
@@ -1,24 +1,18 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components/native';
-import { Text } from 'react-native';
-import { color } from 'styled-system';
+import {Text} from 'react-native';
+import {color} from 'styled-system';
 
 const StyledButton = styled.TouchableHighlight`
   ${color}
 `;
 
-const Button = ({
-  onPress = () => {},
-  text,
-  ...otherProps
-}) => {
+const Button = ({onPress = () => {}, text, ...otherProps}) => {
   return (
     <StyledButton {...otherProps}>
-      <Text style={{color: "white", fontSize: 24}}>
-        {text}
-      </Text>
+      <Text style={{color: 'white', fontSize: 24}}>{text}</Text>
     </StyledButton>
-  )
-}
+  );
+};
 
 export default Button;
