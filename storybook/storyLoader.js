0845250abe786e602054d5657d31a2030037cd98
Sangamesh Somawar <sangamesh.somawar@bigbinary.com>
2021-11-10 11:05:34 +0530

    Added initial setup for storybook

---
+++ storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('./stories/Button/Button.stories');
+  require('./stories/Welcome/Welcome.stories');
+}
+
+const stories = [
+  './stories/Button/Button.stories',
+  './stories/Welcome/Welcome.stories',
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
