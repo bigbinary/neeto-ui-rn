a3f7c9f4f5e7be084eec91d379959682d4e07db4
Sangamesh Somawar <sangamesh.somawar@bigbinary.com>
2021-11-15 16:23:20 +0530

    Migrated web to beta version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,13 @@
+const custom = require("../webpack.config.js");
+
 module.exports = {
+  webpackFinal: config => {
+    return {
+      ...config,
+      resolve: { alias: { ...config.resolve.alias, ...custom.resolve.alias } },
+      module: { ...config.module, rules: custom.module.rules },
+    };
+  },
   stories: ["../storybook/**/*.stories.?(ts|tsx|js|jsx)"],
   addons: [
     "@storybook/addon-ondevice-notes",
