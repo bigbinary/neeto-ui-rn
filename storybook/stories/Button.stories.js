aa02563c76f7339ee908b7f17a907ae6e69eff8e
Sangamesh Somawar <sangamesh.somawar@bigbinary.com>
2021-12-01 17:02:19 +0530

    Updated width from string to integer

--- storybook/stories/Button.stories.js
+++ storybook/stories/Button.stories.js
@@ -15,41 +15,41 @@ export default ButtonMetaData;
 export const Buttons = () => {
   return (
     <>
-      <Button my={9} width="200" label="Default is solid" />
-      <Button my={9} width="200" disabled label="Default Disbaled" />
+      <Button my={9} width={200} label="Default is solid" />
+      <Button my={9} width={200} disabled label="Default Disbaled" />
       <Button
         my={9}
-        width="200"
+        width={200}
         variant="solid"
         disbaled
         label="Solid Button"
       />
       <Button
         my={9}
-        width="200"
+        width={200}
         variant="solid"
         disabled
         label="Solid Button Disabled"
       />
-      <Button my={9} width="200" variant="inverse" label="Inverse Button" />
+      <Button my={9} width={200} variant="inverse" label="Inverse Button" />
       <Button
         my={9}
-        width="200"
+        width={200}
         variant="inverse"
         disabled
         label="Invese Button Disabled"
       />
-      <Button my={9} width="200" variant="text" label="Text Button" />
+      <Button my={9} width={200} variant="text" label="Text Button" />
       <Button
         my={9}
-        width="200"
+        width={200}
         variant="text"
         disabled
         label="Text Button Disabled"
       />
       <Button
         my={9}
-        width="200"
+        width={200}
         variant="inverse"
         label="Like"
         RightIcon={() => {
@@ -58,7 +58,7 @@ export const Buttons = () => {
       />
       <Button
         my={9}
-        width="200"
+        width={200}
         variant="inverse"
         label="Subscribe and Follow"
         RightIcon={() => {
@@ -70,7 +70,7 @@ export const Buttons = () => {
       />
       <Button
         my={9}
-        width="200"
+        width={200}
         variant="inverse"
         disabled
         label="Unsubscribe Disabled"
@@ -80,14 +80,14 @@ export const Buttons = () => {
       />
       <Button
         my={9}
-        width="200"
+        width={200}
         bg="background.primary"
         color="font.danger"
         label="Logout"
       />
       <Button
         my={9}
-        width="200"
+        width={200}
         bg="background.primary"
         color="font.danger"
         fontSize="xl"
@@ -95,7 +95,7 @@ export const Buttons = () => {
       />
       <Button
         my={9}
-        width="200"
+        width={200}
         fontFamily="inter400"
         label="Regular inter 400"
       />
