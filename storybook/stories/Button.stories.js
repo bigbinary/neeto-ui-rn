cb0efb5509140f3486686dbd57aeff57afa718dd
Sangamesh Somawar <sangamesh.somawar@bigbinary.com>
2021-12-01 13:49:18 +0530

    Updated Button stories width

--- storybook/stories/Button.stories.js
+++ storybook/stories/Button.stories.js
@@ -15,21 +15,41 @@ export default ButtonMetaData;
 export const Buttons = () => {
   return (
     <>
-      <Button my={9} label="Default is solid" />
-      <Button my={9} disabled label="Default Disbaled" />
-      <Button my={9} variant="solid" disbaled label="Solid Button" />
-      <Button my={9} variant="solid" disabled label="Solid Button Disabled" />
-      <Button my={9} variant="inverse" label="Inverse Button" />
+      <Button my={9} width="200" label="Default is solid" />
+      <Button my={9} width="200" disabled label="Default Disbaled" />
       <Button
         my={9}
+        width="200"
+        variant="solid"
+        disbaled
+        label="Solid Button"
+      />
+      <Button
+        my={9}
+        width="200"
+        variant="solid"
+        disabled
+        label="Solid Button Disabled"
+      />
+      <Button my={9} width="200" variant="inverse" label="Inverse Button" />
+      <Button
+        my={9}
+        width="200"
         variant="inverse"
         disabled
         label="Invese Button Disabled"
       />
-      <Button my={9} variant="text" label="Text Button" />
-      <Button my={9} variant="text" disabled label="Text Button Disabled" />
+      <Button my={9} width="200" variant="text" label="Text Button" />
       <Button
         my={9}
+        width="200"
+        variant="text"
+        disabled
+        label="Text Button Disabled"
+      />
+      <Button
+        my={9}
+        width="200"
         variant="inverse"
         label="Like"
         RightIcon={() => {
@@ -38,6 +58,7 @@ export const Buttons = () => {
       />
       <Button
         my={9}
+        width="200"
         variant="inverse"
         label="Subscribe and Follow"
         RightIcon={() => {
@@ -49,6 +70,7 @@ export const Buttons = () => {
       />
       <Button
         my={9}
+        width="200"
         variant="inverse"
         disabled
         label="Unsubscribe Disabled"
