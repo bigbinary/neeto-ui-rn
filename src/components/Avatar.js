51cf8fee15fe9c152505969b2fa6b784cd023299
Jenipha Rachel <jenipharachel@gmail.com>
2021-12-07 16:04:23 +0530

    added imageUrl to props

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+import { Image } from "react-native";
 
 import { Typography, Container } from "@components";
 
@@ -8,6 +9,7 @@ export const Avatar = ({
   size = 80,
   bgColor = "background.grey200",
   fontColor = "font.grey600",
+  imageUrl,
   ...rest
 }) => {
   const acronym = name
@@ -15,22 +17,40 @@ export const Avatar = ({
     .reduce((response, word) => (response += word.slice(0, 1)), "");
   const fontSize = size / 2 - 8;
 
+  const styles = {
+    profileImage: {
+      height: size,
+      width: size,
+      alignSelf: "center",
+      borderRadius: size / 2,
+    },
+  };
   return (
-    <Container
-      bg={bgColor}
-      width={size}
-      height={size}
-      borderRadius={size / 2}
-      {...rest}
-    >
-      <Typography
-        fontFamily="inter700"
-        fontSize={fontSize > 14 ? fontSize : 14}
-        color={fontColor}
-      >
-        {acronym}
-      </Typography>
-    </Container>
+    <>
+      {imageUrl ? (
+        <Image
+          style={styles.profileImage}
+          source={{ uri: imageUrl }}
+          {...rest}
+        />
+      ) : (
+        <Container
+          bg={bgColor}
+          width={size}
+          height={size}
+          borderRadius={size / 2}
+          {...rest}
+        >
+          <Typography
+            fontFamily="inter700"
+            fontSize={fontSize > 14 ? fontSize : 14}
+            color={fontColor}
+          >
+            {acronym}
+          </Typography>
+        </Container>
+      )}
+    </>
   );
 };
 
@@ -44,4 +64,5 @@ Avatar.propTypes = {
   size: PropTypes.number,
   bgColor: PropTypes.string,
   fontColor: PropTypes.string,
+  imageUrl: PropTypes.string,
 };
--- storybook/stories/Avatar.stories.js
+++ storybook/stories/Avatar.stories.js
@@ -7,31 +7,152 @@ const AvatarStories = {
 };
 export default AvatarStories;
 
-export const Avatars = () => {
+export const TextSizes = () => {
   return (
     <Container flex={1} alignItems="center" justifyContent="center">
       <Container flexDirection="row" alignItems="center">
         <Avatar name="Oliver Smith" m={2} />
-        <Typography fontSize="xl">82 * 82 (default size)</Typography>
+        <Typography fontSize="xl">82 * 82 (Default size)</Typography>
       </Container>
 
       <Container flexDirection="row" alignItems="center">
-        <Avatar
-          size={48}
-          name="Oliver Smith"
-          m={2}
-          bgColor="background.black"
-        />
+        <Avatar size={48} name="Oliver Smith" m={2} />
         <Typography fontSize="l">48 * 48</Typography>
       </Container>
 
       <Container flexDirection="row" alignItems="center">
-        <Avatar size={42} name="Oliver Smith" m={2} fontColor="font.white" />
+        <Avatar size={42} name="Oliver Smith" m={2} />
         <Typography fontSize="m">42 * 42</Typography>
       </Container>
 
       <Container flexDirection="row" alignItems="center">
         <Avatar size={32} name="Oliver Smith" m={2} />
+        <Typography fontSize="s">32 * 32</Typography>
+      </Container>
+    </Container>
+  );
+};
+
+export const TextColors = () => {
+  return (
+    <Container flex={1} alignItems="center" justifyContent="center">
+      <Container flex={1} justifyContent="center">
+        <Container flexDirection="row" alignItems="center">
+          <Avatar size={42} name="Oliver Smith" m={2} fontColor="font.white" />
+          <Typography fontSize="m">Font White</Typography>
+        </Container>
+
+        <Container flexDirection="row" alignItems="center">
+          <Avatar size={42} name="Oliver Smith" m={2} fontColor="font.black" />
+          <Typography fontSize="m">Font Black</Typography>
+        </Container>
+
+        <Container flexDirection="row" alignItems="center">
+          <Avatar
+            size={42}
+            name="Oliver Smith"
+            m={2}
+            fontColor="font.secondary"
+          />
+          <Typography fontSize="m">Font Secondary</Typography>
+        </Container>
+
+        <Container flexDirection="row" alignItems="center">
+          <Avatar size={42} name="Oliver Smith" m={2} fontColor="font.danger" />
+          <Typography fontSize="m">Font Danger</Typography>
+        </Container>
+      </Container>
+    </Container>
+  );
+};
+
+export const TextBGColors = () => {
+  return (
+    <Container flex={1} alignItems="center" justifyContent="center">
+      <Container flex={1} justifyContent="center">
+        <Container flexDirection="row" alignItems="center">
+          <Avatar
+            size={42}
+            name="Oliver Smith"
+            m={2}
+            bgColor="background.white"
+          />
+          <Typography fontSize="m">BG White</Typography>
+        </Container>
+
+        <Container flexDirection="row" alignItems="center">
+          <Avatar
+            size={42}
+            name="Oliver Smith"
+            m={2}
+            bgColor="background.black"
+          />
+          <Typography fontSize="m">BG Black</Typography>
+        </Container>
+
+        <Container flexDirection="row" alignItems="center">
+          <Avatar
+            size={42}
+            name="Oliver Smith"
+            m={2}
+            bgColor="background.secondary"
+          />
+          <Typography fontSize="m">BG Secondary</Typography>
+        </Container>
+
+        <Container flexDirection="row" alignItems="center">
+          <Avatar
+            size={42}
+            name="Oliver Smith"
+            m={2}
+            bgColor="background.danger"
+          />
+          <Typography fontSize="m">BG Danger</Typography>
+        </Container>
+      </Container>
+    </Container>
+  );
+};
+
+export const ImageSizes = () => {
+  return (
+    <Container flex={1} alignItems="center" justifyContent="center">
+      <Container flexDirection="row" alignItems="center">
+        <Avatar
+          imageUrl="https://www.himalmag.com/wp-content/uploads/2019/07/sample-profile-picture.png"
+          name="Emma Watson"
+          margin={2}
+        />
+        <Typography fontSize="xl">82 * 82 (Default size)</Typography>
+      </Container>
+
+      <Container flexDirection="row" alignItems="center">
+        <Avatar
+          size={48}
+          imageUrl="https://www.himalmag.com/wp-content/uploads/2019/07/sample-profile-picture.png"
+          name="Emma Watson"
+          margin={3}
+        />
+        <Typography fontSize="l">48 * 48</Typography>
+      </Container>
+
+      <Container flexDirection="row" alignItems="center">
+        <Avatar
+          size={42}
+          imageUrl="https://www.himalmag.com/wp-content/uploads/2019/07/sample-profile-picture.png"
+          name="Emma Watson"
+          margin={3}
+        />
+        <Typography fontSize="m">42 * 42</Typography>
+      </Container>
+
+      <Container flexDirection="row" alignItems="center">
+        <Avatar
+          size={32}
+          imageUrl="https://www.himalmag.com/wp-content/uploads/2019/07/sample-profile-picture.png"
+          name="Emma Watson"
+          margin={3}
+        />
         <Typography fontSize="s">32 * 32</Typography>
       </Container>
     </Container>
