48d1ab48e54b98fbaa2cf9a20846fa36b162b086
Jenipha Rachel <jenipharachel@gmail.com>
2021-12-08 14:21:31 +0530

    story update for toggle switch

--- storybook/stories/ToggleSwitch.stories.js
+++ storybook/stories/ToggleSwitch.stories.js
@@ -4,29 +4,56 @@ import { Container, ToggleSwitch } from "@components";
 const ToggleSwitchStories = {
   title: "ToggleSwitches",
   component: ToggleSwitch,
+  argTypes: {
+    onValueChange: { action: "pressed the button" },
+  },
+  args: {
+    disabled: false,
+  },
   parameters: {
     notes: "Checkout the different props in controls section",
   },
 };
 
-const Template = args => {
-  const [switchOne, setSwitchOne] = useState(true);
+export default ToggleSwitchStories;
+
+export const ToggleSwitchDemo = args => {
+  const [demoSwitch, setDemoSwitch] = useState(true);
 
   return (
     <Container flex={1} justifyContent="center" alignItems="center">
       <ToggleSwitch
         {...args}
-        value={switchOne}
-        onValueChange={() => setSwitchOne(prevValue => !prevValue)}
-        label="Conversation assigned to my group"
+        value={demoSwitch}
+        onValueChange={() => setDemoSwitch(prevValue => !prevValue)}
+        label="Toggle Switch Demo"
       />
     </Container>
   );
 };
 
-export const ToggleSwitches = Template.bind({});
-ToggleSwitches.args = {
-  disabled: false,
-};
+export const ToggleSwitches = () => {
+  const [switchOne, setSwitchOne] = useState(true);
 
-export default ToggleSwitchStories;
+  return (
+    <Container flex={1} justifyContent="center" alignItems="center">
+      <ToggleSwitch
+        value={switchOne}
+        onValueChange={() => setSwitchOne(prevValue => !prevValue)}
+        label="Conversation assigned to my group"
+      />
+      <ToggleSwitch
+        mt={3}
+        value={true}
+        label="Disabled switched on toggle button"
+        disabled
+      />
+      <ToggleSwitch
+        mt={3}
+        value={false}
+        label="Disabled switched off toggle button"
+        disabled
+      />
+    </Container>
+  );
+};
