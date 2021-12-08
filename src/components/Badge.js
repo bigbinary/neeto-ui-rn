c3ddc5b756427ea2674fece45e2a9a082ad9d1b1
jayanth-karri22 <jayanth.karri@bigbinary.com>
2021-12-08 14:24:32 +0530

    updated docs and snapshot

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,6 +2,39 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Typography, Container } from "@components";
 
+/**
+ *
+ * This component supports below props categories from [styled-system ](/styled-system).
+ * <ul>
+ * <li>flexbox</li>
+ * <li>space</li>
+ * <li>border</li>
+ * <li>color</li>
+ * <li>layout</li>
+ * </ul>
+ *
+ *  ## Usage
+ * ```js
+ * import * as React from 'react';
+ * import { Badge, Container } from '@bigbinary/neetoui-rn';
+ *
+ * export default function Main() {
+ *  return (
+ *    <Container>
+ *       <Badge
+ *          content="test"
+ *          size={32}
+ *          fontSize="12px"
+ *          badgeColor="background.secondary"
+ *          color="font.secondary"
+ *        />
+ *    </Container>
+ *  );
+ * }
+ * ```
+ *
+ */
+
 export const Badge = ({
   badgeColor = "background.base",
   content,
@@ -31,12 +64,28 @@ export const Badge = ({
 };
 
 Badge.propTypes = {
+  /**
+   * Custom badge content color.
+   */
   color: PropTypes.string,
+  /**
+   * Custom badge background color.
+   */
   badgeColor: PropTypes.string,
+  /**
+   * To change content of the badge
+   */
   content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * Custom badge size.
+   */
   size: PropTypes.number,
-  textStyles: PropTypes.object,
-  borderRadius: PropTypes.number,
+  /**
+   * Custom font-size for badge content.
+   */
   fontSize: PropTypes.string,
+  /**
+   * Custom font-family for badge content.
+   */
   fontFamily: PropTypes.string,
 };
