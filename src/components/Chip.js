9766dce07c5c0ab9d987932b8a6c8756ce2395ff
T Sudhish Nair <sudhish.nair@bigbinary.com>
2021-12-23 19:03:38 +0530

    Added documentation and controls for chip component. (#146)
    * Added documentation for chip
    
    * Added controls and actions for chip component
    
    * Added controls and actions for chip component
    
    * Fixed docs error,  corrected import statement in example, Updated snapshots.
    
    Co-authored-by: Sangamesh Somawar <sangamesh.somawar@bigbinary.com>

--- src/components/Chip.js
+++ src/components/Chip.js
@@ -25,6 +25,40 @@ const variantStyleObj = {
   },
 };
 
+/**
+ *
+ * Chips are compact elements that represent an input, attribute, or action.
+ *
+ * <div class="screenshots">
+ *   <img src="screenshots/chip/chip.png" />
+ * </div>
+ *
+ *   ## Usage
+ * ```js
+ * import * as React from 'react';
+ * import { Chip, Container, Typography } from '@bigbinary/neetoui-rn';
+ *
+ * export default function Main(){
+ *  return (
+ *   <Container>
+ *     <Chip
+ *      label="Outlined Chip"
+ *      LeftIcon={() => <Typography>📣</Typography>}
+ *      variant="outlined"
+ *      onChipPress={() => {
+ *       alert("Chip pressed");
+ *      }}
+ *      onClose={() => {
+ *        alert("On Close clicked");
+ *       }}
+ *    />
+ *   </Container>
+ *  );
+ * }
+ * ```
+ *
+ */
+
 export const Chip = ({
   label,
   LeftIcon,
@@ -81,15 +115,48 @@ Chip.defaultProps = {
 };
 
 Chip.propTypes = {
+  /**
+   * Takes the text that needs to be displayed inside the chip.
+   */
   label: PropTypes.string.isRequired,
+  /**
+   * Expects a component. Used to set left Icon or Avatar.
+   */
   LeftIcon: PropTypes.elementType,
+  /**
+   * Supported variant: 'outlined' | 'solid'
+   */
   variant: PropTypes.string,
+  /**
+   * Sets chip text color.
+   */
   labelColor: PropTypes.string,
+  /**
+   * Sets close Icon background color.
+   */
   closeIconBackground: PropTypes.string,
+  /**
+   * Sets close Icon color.
+   */
   closeIconColor: PropTypes.string,
+  /**
+   * Expects a function to execute on close icon press. The close icon appears only when the onClose prop is specified.
+   */
   onClose: PropTypes.func,
-  onChipPress: PropTypes.func,
-  isDisabled: PropTypes.bool,
+  /**
+   * Used to set the size of the close icon.
+   */
   closeIconSize: PropTypes.number,
+  /**
+   * Close icon name can be specified if default one needs to changed. Only supports remixIcons.
+   */
   closeIcon: PropTypes.string,
+  /**
+   * Expects a function to execute on chip press.
+   */
+  onChipPress: PropTypes.func,
+  /**
+   * Whether the chip is disabled. A disabled chip has a overlay and onPress is not called on touch.
+   */
+  isDisabled: PropTypes.bool,
 };
