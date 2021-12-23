5650bc72f0103ac847060158c9822a95d72284ba
T Sudhish Nair <sudhish.nair@bigbinary.com>
2021-12-23 23:45:10 +0530

    Added documentation and controls for button component (#149)
    * Added documentation and controls for button component
    
    * Updated Snapshots
    
    Co-authored-by: Sangamesh Somawar <sangamesh.somawar@bigbinary.com>

--- src/components/Button.js
+++ src/components/Button.js
@@ -21,6 +21,37 @@ export const TouchableOpacity = styled.TouchableOpacity`
   ${color}
 `;
 
+/**
+ *
+ * Buttons are touchable elements used to interact with the screen and to trigger an action.
+ *
+ * <div class="screenshots">
+ *   <img src="screenshots/button/solidButtons.png" />
+ *   <img src="screenshots/button/loaderButtons.png" />
+ * </div>
+ *
+ *  ## Usage
+ * ```js
+ * import * as React from 'react';
+ * import { Button, Container } from '@bigbinary/neetoui-rn';
+ *
+ * export default function Main() {
+ *  return (
+ *    <Container>
+ *      <Button
+ *        my={9}
+ *        width={200}
+ *        variant="solid"
+ *        disabled
+ *        label="Solid Button"
+ *       />
+ *    </Container>
+ *  );
+ * }
+ * ```
+ *
+ */
+
 export const Button = ({
   RightIcon,
   LeftIcon,
@@ -84,10 +115,38 @@ Button.propTypes = {
   ...propTypes.border,
   ...propTypes.layout,
   ...propTypes.color,
+  /**
+   * Takes the text that needs to be displayed inside the button.
+   */
   label: PropTypes.string.isRequired,
+  /**
+   * Supported Type: 'text' | 'inverse' | 'solid'
+   * You can change the mode to adjust the styling.
+   *  <ul>
+   *    <li>text - flat button without background or outline.</li>
+   *    <li>inverse - button with an outline.</li>
+   *    <li>solid - button with a background color.</li>
+   *  </ul>
+   */
+  variant: PropTypes.oneOf(["text", "inverse", "solid"]),
+  /**
+   * Expects a component.
+   */
   RightIcon: PropTypes.elementType,
+  /**
+   * Expects a component.
+   */
   LeftIcon: PropTypes.elementType,
+  /**
+   * Custom text can be specified while loading if default one needs to changed.
+   */
   loadingText: PropTypes.string,
+  /**
+   * Whether to show a loading indicator.
+   */
   isLoading: PropTypes.bool,
+  /**
+   * Expects a component. Is used to override the default loader.
+   */
   Loader: PropTypes.elementType,
 };
--- storybook/stories/Button.stories.js
+++ storybook/stories/Button.stories.js
@@ -1,28 +1,94 @@
 import React from "react";
-import { Button, Typography, Container } from "@components";
 import { ActivityIndicator } from "react-native";
+
+import { Button, Typography, Container } from "@components";
+import { theme } from "@theme";
+
+const variants = ["text", "inverse", "solid"];
+const fontColors = Object.keys(theme.colors.font).map(color => `font.${color}`);
+const backgroundColors = Object.keys(theme.colors.background).map(
+  color => `background.${color}`
+);
 
 const ButtonMetaData = {
   title: "Buttons",
   component: Button,
+  args: {
+    label: "Button Component",
+    variant: variants[1],
+    backgroundColor: backgroundColors[1],
+    color: fontColors[3],
+    disabled: false,
+    isLoading: false,
+    loadingText: "Loading...",
+  },
   argTypes: {
+    label: {
+      control: {
+        type: "text",
+      },
+    },
+    color: {
+      options: fontColors,
+      control: {
+        type: "select",
+      },
+    },
+    backgroundColor: {
+      options: backgroundColors,
+      control: {
+        type: "select",
+      },
+    },
+    variant: {
+      options: variants,
+      control: {
+        type: "select",
+      },
+    },
+    disabled: {
+      control: {
+        type: "boolean",
+      },
+    },
+    isLoading: {
+      control: {
+        type: "boolean",
+      },
+    },
+    loadingText: {
+      control: {
+        type: "text",
+      },
+    },
     onPress: { action: "pressed the button" },
   },
-  args: {},
+  parameters: {
+    notes:
+      "Checkout the supported control/actions in control and actions section respectively.",
+  },
 };
 
 export default ButtonMetaData;
 
+export const ButtonDemo = args => {
+  return (
+    <Container flex={1} alignItems="center" justifyContent="center">
+      <Button {...args} />
+    </Container>
+  );
+};
+
 export const Buttons = () => {
   return (
-    <>
+    <Container alignItems="center">
       <Button my={9} width={200} label="Default is solid" />
-      <Button my={9} width={200} disabled label="Default Disbaled" />
+      <Button my={9} width={200} disabled label="Default Disabled" />
       <Button
         my={9}
         width={200}
         variant="solid"
-        disbaled
+        disabled
         label="Solid Button"
       />
       <Button
@@ -155,6 +221,6 @@ export const Buttons = () => {
           );
         }}
       />
-    </>
+    </Container>
   );
 };
