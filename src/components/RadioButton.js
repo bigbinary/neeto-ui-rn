67c59d369ca100a5253fa58eff84c58fd3cea653
Sangamesh Somawar <sangamesh.somawar@bigbinary.com>
2022-04-07 17:24:39 +0530

    Finetuned props and storybook example.

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Container, Touchable, Typography } from "@components";
-const LABELPOSITIONS = ["left", "right"];
+const LABEL_POSITIONS = ["left", "right"];
 
 const LabelComponent = ({ label, labelComponent, labelProp }) => {
   return (
@@ -18,45 +18,37 @@ const LabelComponent = ({ label, labelComponent, labelProp }) => {
  *
  * This component supports below props categories from [styled-system ](/styled-system).
  * <ul>
- * <li>outerWidth</li>
- * <li>innerWidth</li>
- * <li>containerBg</li>
- * <li>innerActive</li>
- * <li>innerInActiveBg</li>
- * <li>onSelect</li>
- * <li>selected</li>
- * <li>innerOuterCircleDelta</li>
- * <li>label</li>
- * <li>labelComponent</li>
- * <li>labelProp</li>
- * <li>containerProp</li>
- * <li>labelPosition</li>
- * <li>disabled</li>
+ * <li>flexbox</li>
+ * <li>space</li>
+ * <li>border</li>
+ * <li>layout</li>
+ * <li>color</li>
+ * <li>buttonStyle</li>
  * </ul>
  *
  *  ## Usage
  * ```js
- * import React, { useEffect, useState } from "react";
- * import { Container, RadioButton, Typography } from "@bigbinary/neetoui-rn";
+ * import * as React from "react";
+ * import { Container, RadioButton } from "@bigbinary/neetoui-rn";
  *
  * export default function Main() {
- * useEffect(() => {
- * // console.log("selected is ", selected);
- * }, [selected]);
- *  return (
- *    <Container>
- *     <RadioButton
- *      selected={selected === 0}
- *      onSelect={() => setSelected(0)}
- *      label="option label 1"
- *      labelPosition="left"
- *     />
- *    </Container>
- *  );
- * }
+ *  const [selected, setSelected] = useState(false);
+ *
+ *   return (
+ *     <Container>
+ *      <RadioButton
+ *       selected={selected}
+ *       onSelect={() => setSelected(!selected)}
+ *       label="option label 1"
+ *       labelPosition="left"
+ *      />
+ *     </Container>
+ *   );
+ *  }
  * ```
  *
  */
+
 export const RadioButton = ({
   outerWidth = 20,
   innerWidth = 10,
@@ -69,76 +61,98 @@ export const RadioButton = ({
   label = "Option",
   labelComponent = null,
   labelProp = {},
-  containerProp = {},
   labelPosition = "right",
   disabled = false,
+  ...rest
 }) => {
   return (
     <Touchable
       disabled={disabled}
       opacity={disabled ? 0.5 : 1}
       onPress={onSelect}
+      flexDirection="row"
+      alignItems="center"
+      py={1}
+      {...rest}
     >
-      <Container flexDirection="row" alignItems="center" {...containerProp}>
-        {labelPosition === LABELPOSITIONS[0] ? (
-          <LabelComponent
-            label={label}
-            labelComponent={labelComponent}
-            labelProp={labelProp}
-          />
-        ) : null}
+      {labelPosition === LABEL_POSITIONS[0] ? (
+        <LabelComponent
+          label={label}
+          labelComponent={labelComponent}
+          labelProp={labelProp}
+        />
+      ) : null}
+      <Container
+        bg={containerBg}
+        height={outerWidth}
+        width={outerWidth}
+        borderRadius={outerWidth / 2}
+        justifyContent="center"
+        alignItems="center"
+      >
         <Container
-          bg={containerBg}
-          height={outerWidth}
-          width={outerWidth}
-          borderRadius={outerWidth / 2}
+          height={innerWidth + innerOuterCircleDelta}
+          width={innerWidth + innerOuterCircleDelta}
+          borderRadius={(innerWidth + innerOuterCircleDelta) / 2}
+          bg="background.white"
           justifyContent="center"
           alignItems="center"
         >
           <Container
-            height={innerWidth + innerOuterCircleDelta}
-            width={innerWidth + innerOuterCircleDelta}
-            borderRadius={(innerWidth + innerOuterCircleDelta) / 2}
-            bg="background.white"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Container
-              bg={selected ? innerActiveBg : innerInActiveBg}
-              height={innerWidth}
-              width={innerWidth}
-              borderRadius={innerWidth / 2}
-              m={1}
-            />
-          </Container>
-        </Container>
-        {labelPosition === LABELPOSITIONS[1] ? (
-          <LabelComponent
-            label={label}
-            labelComponent={labelComponent}
-            labelProp={labelProp}
+            bg={selected ? innerActiveBg : innerInActiveBg}
+            height={innerWidth}
+            width={innerWidth}
+            borderRadius={innerWidth / 2}
+            m={1}
           />
-        ) : null}
+        </Container>
       </Container>
+      {labelPosition === LABEL_POSITIONS[1] ? (
+        <LabelComponent
+          label={label}
+          labelComponent={labelComponent}
+          labelProp={labelProp}
+        />
+      ) : null}
     </Touchable>
   );
 };
 
 RadioButton.propTypes = {
+  /**
+   * Function to execute on press.
+   */
   onSelect: PropTypes.func.isRequired,
+  /**
+   * Whether radio is selected.
+   */
   selected: PropTypes.any.isRequired,
+  /**
+   * Text label to be shown with radio button.
+   */
+  label: PropTypes.string,
+  /**
+   * Props for the label.
+   */
+  labelProp: PropTypes.object,
+  /**
+   * Label can passed as a component as well.
+   */
+  labelComponent: PropTypes.element,
+  /**
+   * Position of label relative to the radio button. It can be either `left` or `right`
+   */
+  labelPosition: PropTypes.oneOf(LABEL_POSITIONS),
+  /**
+   * Whether radio is disabled.
+   */
+  disabled: PropTypes.bool,
   outerWidth: PropTypes.number,
   innerWidth: PropTypes.number,
   containerBg: PropTypes.string,
   innerActiveBg: PropTypes.string,
   innerInActiveBg: PropTypes.string,
   innerOuterCircleDelta: PropTypes.number,
-  label: PropTypes.string,
-  labelComponent: PropTypes.element,
-  labelProp: PropTypes.object,
-  containerProp: PropTypes.object,
-  labelPosition: PropTypes.oneOf(LABELPOSITIONS),
-  disabled: PropTypes.bool,
 };
 
 LabelComponent.propTypes = {
--- storybook/stories/RadioButton.stories.js
+++ storybook/stories/RadioButton.stories.js
@@ -5,13 +5,55 @@ import { Container, RadioButton, Typography } from "@components";
 const RadioButtonStories = {
   title: "Radio Button",
   component: RadioButtonStories,
-  argTypes: {},
-  args: {},
+  argTypes: {
+    labelPosition: {
+      options: ["left", "right"],
+      control: {
+        type: "select",
+      },
+    },
+    disabled: {
+      control: {
+        type: "boolean",
+      },
+    },
+    selected: {
+      control: {
+        type: "boolean",
+      },
+    },
+    label: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    label: "Hello",
+    disabled: false,
+    selected: false,
+    labelPosition: "right",
+  },
 };
 
 export default RadioButtonStories;
 
-export const RadioButtonComponent = () => {
+export const RadioButtonDemo = args => {
+  const { label, selected, disabled, labelPosition } = args;
+  return (
+    <Container>
+      <RadioButton
+        selected={selected}
+        onSelect={() => {}}
+        label={label}
+        labelPosition={labelPosition}
+        disabled={disabled}
+      />
+    </Container>
+  );
+};
+
+export const RadioButtons = () => {
   const [selected, setSelected] = useState(0);
 
   useEffect(() => {
@@ -23,24 +65,30 @@ export const RadioButtonComponent = () => {
       <RadioButton
         selected={selected === 0}
         onSelect={() => setSelected(0)}
-        label="option label 1"
+        label="Left label"
         labelPosition="left"
       />
       <RadioButton
         selected={selected === 1}
         onSelect={() => setSelected(1)}
-        labelComponent={
-          <Typography color="font.grey200" fontFamily="inter700">
-            Option 1
-          </Typography>
-        }
-        containerProp={{ my: 5 }}
+        label="Right label"
+        labelPosition="right"
+      />
+      <RadioButton
+        selected={selected === 3}
+        onSelect={() => setSelected(3)}
+        disabled
+        label="Disabled"
       />
       <RadioButton
         selected={selected === 2}
         onSelect={() => setSelected(2)}
-        labelProp={{ fontFamily: "inter700", color: "font.danger" }}
-        disabled
+        labelComponent={
+          <Typography color="font.grey200" mx={2} fontFamily="inter700">
+            With custom padding and color
+          </Typography>
+        }
+        my={5}
       />
     </Container>
   );
