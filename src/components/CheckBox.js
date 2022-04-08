10445a423571150a4ff5d1686c4f68440d671ca9
INJAS <injas.mtp@bigbinary.com>
2022-04-08 09:53:02 +0530

    Updated props and props documentation

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -2,21 +2,22 @@ import React from "react";
 import Proptypes from "prop-types";
 
 import { Container, Typography, Touchable } from "@components";
-
+const LABEL_POSITIONS = ["left", "right"];
 /**
  *
  * This component supports below props categories from [styled-system ](/styled-system).
  * <ul>
- * <li>onSelect</li>
- * <li>selected</li>
- * <li>checkedComponent</li>
- * <li>containerStyle</li>
- * <li>checkedItemStyle</li>
+ * <li>flexbox</li>
+ * <li>space</li>
+ * <li>border</li>
+ * <li>layout</li>
+ * <li>color</li>
+ * <li>buttonStyle</li>
  * </ul>
  *
  *  ## Usage
  * ```js
- * import React, { useEffect, useState } from "react";
+ * import * as React from "react";
  * import { Container, CheckBox, Typography } from "@bigbinary/neetoui-rn";
  *
  * export default function Main() {
@@ -32,6 +33,7 @@ import { Container, Typography, Touchable } from "@components";
  *    }
  *    setSelectedValues(oldData);
  *  };
+ *
  *  return (
  *    <Container>
  *      <Container flexDirection="row" my={2}>
@@ -50,39 +52,114 @@ import { Container, Typography, Touchable } from "@components";
  * ```
  *
  */
+
+const LabelComponent = ({ label, labelComponent, labelProp }) => {
+  return labelComponent || <Typography {...labelProp}>{label}</Typography>;
+};
+
 export const CheckBox = ({
   selected = false,
   onSelect = () => {},
   checkedComponent = null,
-  containerStyle = {},
-  checkedItemStyle = {},
+  checkedItemProp = {},
+  disabled = false,
+  label = "Option",
+  labelComponent = null,
+  labelProp = {},
+  labelPosition = "right",
+  checkboxContainerProp = {},
+  ...rest
 }) => {
   return (
     <Touchable
+      disabled={disabled}
       onPress={onSelect}
-      borderColor="background.base"
-      borderWidth={1}
-      borderRadius={5}
-      justifyContent="center"
+      opacity={disabled ? 0.5 : 1}
+      flexDirection="row"
       alignItems="center"
-      width={20}
-      height={20}
-      mr={2}
-      {...containerStyle}
+      {...rest}
     >
-      {selected && (
-        <Container>
-          {checkedComponent || <Typography {...checkedItemStyle}>✓</Typography>}
-        </Container>
-      )}
+      {labelPosition === LABEL_POSITIONS[0] ? (
+        <LabelComponent
+          label={label}
+          labelComponent={labelComponent}
+          labelProp={{ mr: 2, ...labelProp }}
+        />
+      ) : null}
+      <Container
+        borderColor="background.base"
+        borderWidth={1}
+        borderRadius={5}
+        justifyContent="center"
+        alignItems="center"
+        width={20}
+        height={20}
+        {...checkboxContainerProp}
+      >
+        {selected && (
+          <Container>
+            {checkedComponent || (
+              <Typography {...checkedItemProp}>✓</Typography>
+            )}
+          </Container>
+        )}
+      </Container>
+      {labelPosition === LABEL_POSITIONS[1] ? (
+        <LabelComponent
+          label={label}
+          labelComponent={labelComponent}
+          labelProp={{ ml: 2, ...labelProp }}
+        />
+      ) : null}
     </Touchable>
   );
 };
 
 CheckBox.propTypes = {
+  /**
+   * Whether checkbox is selected.
+   */
   selected: Proptypes.bool.isRequired,
+  /**
+   * Function to execute on press.
+   */
   onSelect: Proptypes.func.isRequired,
+  /**
+   * To disable the component.
+   */
+  disabled: Proptypes.bool,
+  /**
+   * Render custom checked component.
+   */
   checkedComponent: Proptypes.element,
-  containerStyle: Proptypes.object,
-  checkedItemStyle: Proptypes.object,
+  /**
+   * Check mark style.
+   */
+  checkedItemProp: Proptypes.object,
+  /**
+   * Text label to be shown with radio button.
+   */
+  label: Proptypes.string,
+  /**
+   * Props for the label.
+   */
+  labelProp: Proptypes.object,
+  /**
+   * Label can passed as a component as well.
+   */
+  labelComponent: Proptypes.element,
+  /**
+   * Position of label relative to the radio button. It can be either `left` or `right`
+   */
+  labelPosition: Proptypes.oneOf(LABEL_POSITIONS),
+  /**
+   * Extra param to customize checkbox container
+   */
+  checkboxContainerProp: Proptypes.object,
+};
+
+LabelComponent.propTypes = {
+  label: Proptypes.string,
+  labelComponent: Proptypes.element,
+  labelProp: Proptypes.object,
 };
