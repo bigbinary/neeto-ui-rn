29a74bd922ea01893a0de6b0b002f998e6d0fb17
Jenipha Rachel <jenipharachel@gmail.com>
2022-04-26 12:59:28 +0530

    Divider line docs

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -4,6 +4,26 @@ import styled from "styled-components/native";
 import propTypes from "@styled-system/prop-types";
 import PropTypes from "prop-types";
 
+/**
+ *
+ * Divider is a separator line that can be used between two different sections.
+ *
+ *   ## Usage
+ * ```js
+ * import * as React from 'react';
+ * import { Divider } from '@bigbinary/neetoui-rn';
+ *
+ * export default function Main(){
+ *  return (
+ *   <Container>
+ *     <Divider flex={1} height={1}/>
+ *   </Container>
+ *  );
+ * }
+ * ```
+ *
+ */
+
 const View = styled.View`
   ${flexbox}
   ${space}
@@ -24,5 +44,12 @@ Divider.propTypes = {
   ...propTypes.space,
   ...propTypes.color,
   ...propTypes.layout,
-  children: PropTypes.node,
+  /**
+   * To specify the height of the divider line
+   */
+  height: PropTypes.number,
+  /**
+   * To specify the width of the divider line
+   */
+  width: PropTypes.number,
 };
