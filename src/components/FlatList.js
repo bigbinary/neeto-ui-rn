88bee549b301e7ce153a83164738601379c5b211
Sangamesh Somawar <sangamesh.somawar@bigbinary.com>
2022-04-26 13:54:30 +0530

    fixed imports

--- src/components/FlatList.js
+++ src/components/FlatList.js
@@ -1,5 +1,5 @@
 import { flexbox, space, border, color, layout } from "styled-system";
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 export const FlatList = styled.FlatList`
   ${flexbox}
