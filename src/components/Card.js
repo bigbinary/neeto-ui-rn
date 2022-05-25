1dde5d61ceb596921dae280791a9a20fae9c0a59
Sangamesh Somawar <93119254+sangameshsomawar@users.noreply.github.com>
2022-05-26 02:01:01 +0530

    Added support of TouchableOpacity to card component. (#251)
    * Added support of pressable to component.
    
    * Replaced pressable with TouchableOpacity
    
    * showing opacity based on onPress

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import * as React from "react";
-import { Container } from "@components";
 import PropTypes from "prop-types";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { flexbox, space, border, color, layout } from "styled-system";
+
 import { getShadowStyles } from "../utils/utils";
 
 /**
@@ -37,14 +39,26 @@ import { getShadowStyles } from "../utils/utils";
  *
  */
 
+const StyledTouchableOpacity = styled(TouchableOpacity)`
+  ${flexbox}
+  ${space}
+ ${border}
+ ${color}
+ ${layout}
+`;
+
 export const Card = ({ children, elevation, ...rest }) => {
   const shadowStyles = elevation ? getShadowStyles(elevation) : {};
 
   const cardStyles = { ...styles.defaultShadows, ...shadowStyles };
   return (
-    <Container style={cardStyles} {...rest}>
+    <StyledTouchableOpacity
+      style={cardStyles}
+      activeOpacity={rest.onPress ? 0.2 : 1}
+      {...rest}
+    >
       {children}
-    </Container>
+    </StyledTouchableOpacity>
   );
 };
 
