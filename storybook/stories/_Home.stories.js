a8abda10c9c704981b2a81d044a6044247847bc7
Jenipha Rachel <35962711+jenipharachel@users.noreply.github.com>
2022-07-12 13:19:01 +0530

    Updated theme and font (#271)
    * modified fonts
    
    * updated themes
    
    * updated components with font sizes
    
    * updated stories and docs
    
    * updated docs

--- storybook/stories/_Home.stories.js
+++ storybook/stories/_Home.stories.js
@@ -9,7 +9,7 @@ export default TypographyStories;
 export const Home = () => {
   return (
     <Container flex={1} alignItems="center" justifyContent="center">
-      <Typography textAlign="center" fontSize="xxl">
+      <Typography textAlign="center" fontSize="2xl">
         Please open navigator from the bottom left tab and select a story to
         preview.
       </Typography>
