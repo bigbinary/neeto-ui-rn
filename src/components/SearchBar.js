cecfb4e0d4a76eee1a2d3b68e32c5a8a0f7adab9
admin <admin@admins-MacBook-Pro.local>
2022-07-20 16:37:14 +0530

    Added animation to the searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,50 +1,81 @@
-import React, { useContext, useEffect, useState } from "react";
-import { TextInput } from "react-native";
-import { ThemeContext } from "styled-components/native";
+import React, { useEffect, useRef, useState } from "react";
+import { TextInput, Animated, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 import Icon from "react-native-remix-icon";
 
 import { Container, Button } from "@components";
 import { useDebounce } from "@hooks";
+import { theme } from "@theme";
 
 export const SearchBar = props => {
   const { placeholder = "Search", onChangeHandle = () => {}, ...rest } = props;
-  const [searchText, setSearchText] = useState("");
+  const inputRef = useRef();
   const debouncedSearchTextValue = useDebounce(searchText, 1000);
-  const theme = useContext(ThemeContext);
+  const [searchText, setSearchText] = useState("");
+  const searchAnimationController = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     onChangeHandle(debouncedSearchTextValue);
   }, [debouncedSearchTextValue, onChangeHandle]);
 
+  const Animate = val => {
+    Animated.timing(searchAnimationController, {
+      toValue: val,
+      duration: 350,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const searchInputWidth = searchAnimationController.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["100%", "85%"],
+  });
+
+  const onCancelHandle = () => {
+    Animate(0);
+    inputRef.current.blur();
+  };
+
   return (
-    <Container flexDirection="row" alignItems="center">
-      <Container
-        p={12}
-        borderWidth={1}
-        borderColor="border.grey400"
-        borderRadius={8}
-        flexDirection="row"
-        flexGrow={1}
+    <Container flexDirection="row" alignItems="center" flex={1}>
+      <Animated.View
+        style={{
+          ...styles.inputContainerStyles,
+          width: searchInputWidth,
+        }}
       >
         <Container pr={10}>
           <Icon
             name="ri-search-line"
             size={20}
-            color={theme.colors.font.primary}
+            color={theme.colors.font.grey600}
           />
         </Container>
         <TextInput
+          ref={inputRef}
           value={searchText}
           onChangeText={setSearchText}
+          onFocus={() => {
+            Animate(1);
+          }}
+          onBlur={() => {
+            if (!searchText) Animate(0);
+          }}
           placeholder={placeholder}
           fontSize={16}
+          flex={1}
           placeholderTextColor={theme.colors.font.grey600}
           color="font.primary"
           {...rest.searchbarProps}
         />
-      </Container>
-      <Button variant="text" label="Cancel" onPress />
+      </Animated.View>
+      <Animated.View
+        style={{
+          opacity: searchAnimationController,
+        }}
+      >
+        <Button variant="text" label="Cancel" onPress={onCancelHandle} />
+      </Animated.View>
     </Container>
   );
 };
@@ -53,3 +84,13 @@ SearchBar.propTypes = {
   placeholder: PropTypes.string.isRequired,
   onChangeHandle: PropTypes.func.isRequired,
 };
+
+const styles = StyleSheet.create({
+  inputContainerStyles: {
+    padding: 12,
+    borderWidth: 1,
+    borderColor: theme.colors.border.grey400,
+    borderRadius: 8,
+    flexDirection: "row",
+  },
+});
