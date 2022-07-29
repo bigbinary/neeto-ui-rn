e35106c5f148d48eef31ed9694b434e7d6d301ca
INJAS <87293196+injas-bigbinary@users.noreply.github.com>
2022-07-29 14:56:16 +0530

    fixed text overflow (#328)
    * fixed text overflow
    
    * added close button
    
    * Fixed toast message fontSize

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -5,14 +5,15 @@ import { defaultToasterConfig } from "@config";
 import { useContext } from "react";
 import { ThemeContext } from "styled-components/native";
 import { useWindowDimensions } from "react-native";
+import CloseIcon from "react-native-remix-icon";
 
 import SuccessIcon from "@assets/icons/success.svg";
 import WarningIcon from "@assets/icons/warning.svg";
 import InfoIcon from "@assets/icons/info.svg";
 import ErrorIcon from "@assets/icons/error.svg";
-import { Container, Typography } from "@components";
+import { Container, Typography, Touchable } from "@components";
 
-const ToastComponent = ({ type, text1, text2 }) => {
+const ToastComponent = ({ type, text1, text2, hide }) => {
   const theme = useContext(ThemeContext);
   const { width } = useWindowDimensions();
   const Icon = () => {
@@ -30,7 +31,7 @@ const ToastComponent = ({ type, text1, text2 }) => {
   return (
     <Container
       width={width - 20}
-      height={60}
+      minHeight={60}
       borderRadius={16}
       bg={theme.colors.background.secondary}
       flexDirection="row"
@@ -49,11 +50,11 @@ const ToastComponent = ({ type, text1, text2 }) => {
         <Icon />
       </Container>
 
-      <Container ml={10}>
+      <Container flex={1} ml={10} py={3}>
         {text1 && (
           <Typography
             fontFamily={theme.fonts.sf600}
-            fontSize="m"
+            fontSize="l"
             fontColor={theme.colors.font.primary}
           >
             {text1}
@@ -69,6 +70,17 @@ const ToastComponent = ({ type, text1, text2 }) => {
           </Typography>
         )}
       </Container>
+      <Touchable
+        onPress={hide}
+        hitSlop={{
+          top: 5,
+          right: 5,
+          bottom: 5,
+          left: 5,
+        }}
+      >
+        <CloseIcon name="ri-close-line" color={theme.colors.font.grey500} />
+      </Touchable>
     </Container>
   );
 };
@@ -77,6 +89,7 @@ ToastComponent.propTypes = {
   type: PropTypes.string,
   text1: PropTypes.string,
   text2: PropTypes.string,
+  hide: PropTypes.func,
 };
 
 /**
