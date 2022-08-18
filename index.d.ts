f3bf058001aa4b506df5d024304e165e58e98367
Kamolesh Mondal <sedateddev@gmail.com>
2022-08-18 16:35:33 +0530

    Added types for some components

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel from "react-native-snap-carousel";
 import {
   FlexboxProps,
   SpaceProps,
@@ -8,21 +9,46 @@ import {
   PositionProps,
   TypographyProps,
 } from "styled-system";
+import { ViewProps as RNViewProps } from "react-native";
 import { ModalProps } from "react-native-modal";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { CalendarProps } from "react-native-calendars";
 
-import { theme } from "./src/theme";
+import { theme as themeDef } from "./src/theme";
+import { BUTTON_VARIANTS } from "./src/components/Button";
 
-interface ViewProps
+type addPrefixToObject<
+  TObject extends object,
+  TPrefix extends string
+> = `${TPrefix}${keyof TObject}`;
+
+const theme: {
+  colors: {
+    font: addPrefixToObject<typeof themeDef.colors.font, "font.">;
+    background: addPrefixToObject<
+      typeof themeDef.colors.background,
+      "background."
+    >;
+    border: addPrefixToObject<typeof themeDef.colors.border, "border.">;
+    toast: addPrefixToObject<typeof themeDef.colors.toast, "toast.">;
+  };
+  fonts: keyof typeof themeDef.fonts;
+  fontSizes: keyof typeof themeDef.fontSizes;
+};
+
+interface StyleProps
   extends FlexboxProps,
     SpaceProps,
     BorderProps,
     ColorProps,
     LayoutProps,
-    PositionProps {
+    PositionProps {}
+
+interface ViewProps extends RNViewProps, StyleProps {
   children?: React.ReactNode;
 }
 
-interface TextProps extends TypographyProps {}
+interface TextProps extends StyleProps, TypographyProps {}
 
 interface AccordianProps extends ViewProps {
   header?: () => void;
@@ -34,12 +60,14 @@ interface AccordianProps extends ViewProps {
     size?: number;
     color?: string;
   };
+  position?: "top" | "bottom";
 }
 
 type AlertProps = {};
 type AlertButton = {
   label: string;
   onPress?: () => void;
+  variant?: typeof BUTTON_VARIANTS[keyof typeof BUTTON_VARIANTS];
 };
 type AlertShowParams = {
   title?: string;
@@ -52,29 +80,28 @@ type AlertShowParams = {
 interface AvatarProps extends ViewProps {
   name: string;
   variant?: "small" | "medium";
-  bgColor?: keyof typeof theme.colors.background;
-  fontColor?: keyof typeof theme.colors.font;
+  bgColor?: typeof theme.colors.background;
+  fontColor?: typeof theme.colors.font;
   imageUrl?: string;
 }
 
 interface BadgeProps extends ViewProps {
-  badgeColor?: keyof typeof theme.colors.background;
-  content: string | number;
+  badgeColor?: typeof theme.colors.background;
+  content?: string | number;
   size?: number;
-  fontSize?: keyof typeof theme.fontSizes;
-  fontFamily?: keyof typeof theme.fonts;
-  color?: keyof typeof theme.colors.font;
+  fontSize?: typeof theme.fontSizes;
+  fontFamily?: typeof theme.fonts;
+  color?: typeof theme.colors.font;
 }
 
 interface BottomSheetProps extends ViewProps {
-  data: Array<any>;
+  data?: Array<any>;
   title?: string;
   hide?: () => void;
   isVisible: boolean;
   onItemPress?: () => void;
   selectedItemIndex?: number;
-  bg?: keyof typeof theme.colors.background;
-  children?: React.ReactNode;
+  bg?: typeof theme.colors.background;
   titleContainerStyle?: ViewProps;
   titleTextStyle?: TextProps;
   modalParams?: ModalProps;
@@ -84,6 +111,63 @@ interface BottomSheetProps extends ViewProps {
   canSearch?: boolean;
 }
 
+interface ButtonProps extends ViewProps {
+  variant?: typeof BUTTON_VARIANTS[keyof typeof BUTTON_VARIANTS];
+  label: string;
+  labelStyle?: TextProps;
+  RightIcon?: () => void;
+  LeftIcon?: () => void;
+  disabled?: boolean;
+  isLoading?: boolean;
+  loadingText?: string;
+}
+
+type ButtonGroupProps = {
+  activeColor?: typeof theme.colors[keyof typeof theme.colors];
+  inActiveColor?: typeof theme.colors[keyof typeof theme.colors];
+  buttonItems: Array<string>;
+  onPress: () => void;
+  currentActiveBtn: string;
+  wrapperStyle?: StyleProps;
+  buttonTextStyle?: StyleProps;
+  buttonStyle?: StyleProps;
+};
+
+interface CardProps extends ViewProps {
+  elevation?: number;
+}
+
+type CarouselProps = {
+  itemArray: Array<any>;
+  renderItem: () => void;
+  carouselRef: React.RefObject<Carousel>;
+  onSnapToItem: () => void;
+};
+
+interface CheckBoxProps extends ViewProps {
+  checked: boolean;
+  onSelect: () => void;
+  disabled?: boolean;
+  label?: string;
+  checkboxStyle?: StyleProps;
+  checkIconStyle?: any;
+  labelStyle?: TextProps;
+}
+
+type ChipProps = {
+  label: string;
+  LeftIcon?: React.FC;
+  variant?: "outlined" | "solid";
+  labelColor?: typeof theme.colors.font;
+  closeIconBackground?: typeof theme.colors.background;
+  closeIconColor?: string;
+  onClose?: () => void;
+  onChipPress?: () => void;
+  isDisabled?: disabled;
+  closeIconSize?: number;
+  closeIcon?: string;
+};
+
 export const Accordian: React.FC<AccordianProps>;
 export const Alert: React.FC<AlertProps> & {
   show?: (params: AlertShowParams) => void;
@@ -91,4 +175,12 @@ export const Alert: React.FC<AlertProps> & {
 export const Avatar: React.FC<AvatarProps>;
 export const Badge: React.FC<BadgeProps>;
 export const BottomSheet: React.FC<BottomSheetProps>;
+export const BottomTabBar: React.FC<BottomTabBarProps>;
+export const Button: React.FC<ButtonProps>;
+export const ButtonGroup: React.FC<ButtonGroupProps>;
+export const Calendar: React.FC<CalendarProps>;
+export const Card: React.FC<CardProps>;
+export const Carousel: React.FC<CarouselProps>;
+export const CheckBox: React.FC<CheckBoxProps>;
+export const Chip: React.FC<ChipProps>;
 export const Container: React.FC<ViewProps>;
