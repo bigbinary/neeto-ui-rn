74a470531b76888fb22d51fc0b784e0a6eafc654
INJAS <injas.mtp@bigbinary.com>
2022-08-29 14:51:23 +0530

    updated example

--- storybook/stories/MultiSelect.stories.js
+++ storybook/stories/MultiSelect.stories.js
@@ -73,7 +73,7 @@ export const MultiSelects = () => {
       )}
       valueExtractor={item => item?.id}
       labelExtractor={item => item?.name}
-      onBackdropPress={() => {}}
+      // onBackdropPress={() => {}}
       // disabled
       // noResultsLabelContainerStyle={{backgroundColor:"red"}}
       // noResultsLabelStyle={{color:"green"}}
