53d04129bf7750157064476d7974d7dfe10b2c38
Jenipha Rachel <jenipharachel@gmail.com>
2022-09-14 15:51:42 +0530

    default timing for animations

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -210,8 +210,6 @@ export const BottomSheet = ({
       useNativeDriver
       avoidKeyboard
       hideModalContentWhileAnimating
-      animationInTiming={600}
-      animationOutTiming={600}
       onRequestClose={hide}
       {...modalParams}
     >
