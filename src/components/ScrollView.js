f53c06fd872676654f77e39058a8b6eb7cba83ed
Chirag Bhaiji <27726339+chiragbhaiji@users.noreply.github.com>
2023-02-08 17:09:20 +0530

    Add defaults to the ScrollView (#571)

--- src/components/ScrollView.js
+++ src/components/ScrollView.js
@@ -1,7 +1,11 @@
 import styled from "styled-components/native";
 import { flexbox, space, border, color, layout } from "styled-system";
 
-export const ScrollView = styled.ScrollView`
+export const ScrollView = styled.ScrollView.attrs(() => ({
+  keyboardShouldPersistTaps: "handled",
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { flex: 1 },
+}))`
   ${flexbox}
   ${space}
   ${border}
