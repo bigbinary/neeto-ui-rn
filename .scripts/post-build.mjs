ed17bcb554e4e149bebd08cc05c8ead4eccc8065
Paul Sojan <paul.eliassojan@gmail.com>
2024-08-09 15:15:22 +0530

    Updated github workflow

---
+++ .scripts/post-build.mjs
@@ -0,0 +1,27 @@
+import fse from "fs-extra";
+import { omit } from "ramda";
+
+const BUILD_FOLDER = "dist";
+
+// Create new package.json for dist folder
+function createPackageFile() {
+  const packageData = fse.readFileSync("./package.json", "utf8");
+  const packageJSON = omit(
+    ["devDependencies", "lint-staged", "scripts"],
+    JSON.parse(packageData)
+  );
+
+  return fse.writeFile(
+    `./${BUILD_FOLDER}/package.json`,
+    JSON.stringify(packageJSON, null, 2),
+    "utf8"
+  );
+}
+
+function copyToBuildFolder(srcBase, folders) {
+  folders.forEach(folder => {
+    fse.copySync(`${srcBase}/${folder}`, `${BUILD_FOLDER}/${folder}`);
+  });
+}
+
+createPackageFile();
